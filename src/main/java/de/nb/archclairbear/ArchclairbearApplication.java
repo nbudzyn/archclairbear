a4b1448fd1a2160828a9d22c726ca986d4e8f2f4
nbudzyn <nbudzyn@gmx.de>
2026-06-08 16:42:40 +0200

    Spring Boot, Web

---
+++ src/main/java/de/nb/archclairbear/ArchclairbearApplication.java
@@ -0,0 +1,13 @@
+package de.nb.archclairbear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchclairbearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchclairbearApplication.class, args);
+	}
+
+}
